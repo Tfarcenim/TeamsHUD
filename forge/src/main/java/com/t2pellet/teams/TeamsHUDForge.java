7f0edf95fabc0f644221dc2681a272c01b8db5aa
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-05-19 23:08:52 -0400

    add forge packet boilerplate

--- forge/src/main/java/com/t2pellet/teams/TeamsHUDForge.java
+++ forge/src/main/java/com/t2pellet/teams/TeamsHUDForge.java
@@ -1,19 +1,28 @@
 package com.t2pellet.teams;
 
+import com.t2pellet.teams.network.PacketHandlerForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TeamsHUD.MODID)
 public class TeamsHUDForge {
     
     public TeamsHUDForge() {
         TeamsHUD.LOGGER.info("Teams forge mod init!");
-
+        IEventBus bus  = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::commonSetup);
         // This method is invoked by the Forge mod loader when it is ready
         // to load your mod. You can access Forge and Common code in this
         // project.
     
         // Use Forge to bootstrap the Common mod.
         TeamsHUD.init();
-        
     }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        PacketHandlerForge.registerPackets();
+    }
+
 }
---
+++ forge/src/main/java/com/t2pellet/teams/network/PacketHandlerForge.java
@@ -0,0 +1,54 @@
+package com.t2pellet.teams.network;
+
+import com.t2pellet.teams.network.client.S2CModPacket;
+import com.t2pellet.teams.network.server.C2SModPacket;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class PacketHandlerForge {
+
+    public static SimpleChannel INSTANCE;
+    static int i = 0;
+
+    public static void registerPackets() {
+        
+    }
+
+    public static <MSG extends S2CModPacket> void clientMessage(Class<MSG> msgClass, BiConsumer<MSG, FriendlyByteBuf> writer, Function<FriendlyByteBuf,MSG> reader) {
+        INSTANCE.registerMessage(i++, msgClass, writer, reader, wrapS2C());
+    }
+
+    public static <MSG extends C2SModPacket> void serverMessage(Class<MSG> msgClass, BiConsumer<MSG, FriendlyByteBuf> writer, Function<FriendlyByteBuf,MSG> reader) {
+        INSTANCE.registerMessage(i++, msgClass, writer, reader, wrapC2S());
+    }
+
+    private static <MSG extends S2CModPacket> BiConsumer<MSG, Supplier<NetworkEvent.Context>> wrapS2C() {
+        return ((msg, contextSupplier) -> {
+            contextSupplier.get().enqueueWork(msg::handleClient);
+            contextSupplier.get().setPacketHandled(true);
+        });
+    }
+
+    private static <MSG extends C2SModPacket> BiConsumer<MSG, Supplier<NetworkEvent.Context>> wrapC2S() {
+        return ((msg, contextSupplier) -> {
+            ServerPlayer player = contextSupplier.get().getSender();
+            contextSupplier.get().enqueueWork(() -> msg.handleServer(player));
+            contextSupplier.get().setPacketHandled(true);
+        });
+    }
+
+    public static <MSG> void sendToClient(MSG packet, ServerPlayer player) {
+        INSTANCE.sendTo(packet, player.connection.connection, NetworkDirection.PLAY_TO_CLIENT);
+    }
+
+    public static <MSG> void sendToServer(MSG packet) {
+        INSTANCE.sendToServer(packet);
+    }
+}
