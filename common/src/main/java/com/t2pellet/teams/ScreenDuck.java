6d756b08534481850fe1407f10ac9bf6b2def53b
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-05-21 17:07:23 -0400

    stupid bug

---
+++ common/src/main/java/com/t2pellet/teams/ScreenDuck.java
@@ -0,0 +1,11 @@
+package com.t2pellet.teams;
+
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Widget;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
+
+public interface ScreenDuck {
+    Button $addButton(Button button);
+
+}
